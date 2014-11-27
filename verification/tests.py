52fda21dcec19cddc2a7fe8d697677208537ce15
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-11-27 17:03:00 +0300

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,93 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
+            "input": ['ACGGCATAACCCTCGA', 3],
+            "answer": "ACGCCCTAATCGGCA"},
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": ['ACGTTGCAACGTAGCT', 4],
+            "answer": "ACGTACGTAGCTTGCA"},
+        {
+            "input": ['CGTAGCA', 1],
+            "answer": "CGTAGCA"},
+        {
+            "input": ['GCTACGG', 7],
+            "answer": "GCTACGG"},
+        {
+            "input": ['GGGCCCTTTAAA', 4],
+            "answer": "CCTTGGGCTAAA"},
+        {
+            "input": ['GCATCTAGCAGCATGCATCGTAGCATCGATCTAGTCAGATCGATGTATAGGCCGCG', 10],
+            "answer": "CGATGTATAGGCATGCATCGTAGCATCGATCTAGTCAGATGCATCTAGCA"},
+        {
+            "input": ['A', 1],
+            "answer": "A"},
+        {
+            "input": ['GGGGGGGGGGG', 2],
+            "answer": "GGGGGGGGGG"},
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
+            "input": ['AAAGATCTGATTGCATAGGAAGCATGCCATCATACGACCCTGCCGGAAGTTACCGGTGGGACGTGACCTTCAACGGTTTGTGTTTCCGC', 46],
+            "answer": "AAAGATCTGATTGCATAGGAAGCATGCCATCATACGACCCTGCCGG"},
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+            "input": ['CGCACTGCCCGCGCGTTATTGCTATGAAGCTAGTGGTCGCCGCCCGCGCTTGGGTTGGATAGTTCATACCCCGGAC', 18],
+            "answer": "TCGCCGCCCGCGCTTGGGCGCACTGCCCGCGCGTTATTGCTATGAAGCTAGTGGTTGGATAGTTCATACCCC"},
+        {
+            "input": ['CGGGCAGGCATTCTTAACGCATTTCAAGCAGCGACTTG', 4],
+            "answer": "CGGGATTTCAGGCATTACGCCAAGCAGCGACTCTTA"},
+        {
+            "input": ['AGACAGACGAGGACCCGTCCAGGGATGGCAAAAGTTCCTCTACGGTCTCGATACCATTAGTTATACGGAGTAT', 1],
+            "answer": "AGACAGACGAGGACCCGTCCAGGGATGGCAAAAGTTCCTCTACGGTCTCGATACCATTAGTTATACGGAGTAT"},
+        {
+            "input": ['GCTCTAGGATCCCGCGCGTTGGAGTCTCCTCCAAATGGGGCACACGGGACACTGCTTCTGA', 12],
+            "answer": "ACACTGCTTCTGCGCGCGTTGGAGGGGGCACACGGGGCTCTAGGATCCTCTCCTCCAAAT"},
+        {
+            "input": ['TAACATGTACGTGCCTCTTTTAGCCTTTACGCTAGATTGCTACATAAGTGCGCAAAACTCGGATGGAGTCTCTA', 13],
+            "answer": "CAAAACTCGGATGTAACATGTACGTGCCTCTTTTAGCCTCTACATAAGTGCGTTACGCTAGATTG"},
+        {
+            "input": ['TTGTGCGCGGTAAAGAACATTTTATCGAGATCTGCCACCGTTGACCCGGCTATTATCGTGATCTCCGGCGGGACACACCAGCTAATCAGTTGA',
+                      19],
+            "answer": "CGTTGACCCGGCTATTATCGTGATCTCCGGCGGGACACTTTTATCGAGATCTGCCACTTGTGCGCGGTAAAGAACA"},
+        {
+            "input": ['CAGGGGCGCCTCGCGAAACCGGAGTTTACGTCTGATTGTGTAGCGGCAACCCAGGCTCCGTTGATA', 4],
+            "answer": "AACCACCCCAGGCCTCGGCGGGAGCGTCAGGCTTTATGATTGTGTCCGGCGATAGCGGCATTGA"},
+        {
+            "input": ['AAGCTGCTGCCACCCATGTCTCGTGTTTGCTTCTACCAGACTGGTCCAGCCAA', 10],
+            "answer": "CACCCATGTCAAGCTGCTGCTCGTGTTTGCCTGGTCCAGCTTCTACCAGA"},
+    ],
+    "Random": [
+
     ]
 }
+
+import random
+
+
+def count_inversion(line):
+    result = 0
+    for i, el in enumerate(line):
+        for sel in line[i + 1:]:
+            result += int(el > sel)
+    return result
+
+
+def golf(sequence, partition_number):
+    subs = [sequence[i * partition_number:(i + 1) * partition_number]
+            for i in range(len(sequence) // partition_number)]
+    sorted_subs = sorted(subs, key=count_inversion)
+    return "".join(sorted_subs)
+
+
+LETTERS = "ACGT"
+
+for __ in range(10):
+    s = "".join(random.choice(LETTERS) for _ in range(random.randint(1, 100)))
+    n = random.randint(1, len(s) // 2)
+    t = [s, n]
+    ans = golf(*t)
+    TESTS["Random"].append({
+        "input": t,
+        "answer": ans})
